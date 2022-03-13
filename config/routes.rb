b0d3567ad30202cf4eb73e6ac967bfac71b15a7f
Vladimir Kozyrev <vkozyrev@anna.money>
2022-03-13 16:37:53 +0300

    convert erb to slim

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  scope module 'web' do
+  scope module: 'web' do
     post 'auth/:provider', to: 'auth#request', as: :auth_request
     get 'auth/:provider/callback', to: 'auth#callback', as: :callback_auth
   end
