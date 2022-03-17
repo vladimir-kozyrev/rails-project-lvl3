2ea71799dcddf20bfe990e867e6af27f8ee47723
Vladimir Kozyrev <vkozyrev@anna.money>
2022-03-17 17:50:09 +0300

    add admin column to users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@
 class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true
+  validates :admin, presence: true
   has_many :bulletins
 
   def self.find_or_create_from_auth(auth)
@@ -10,5 +11,9 @@ class User < ApplicationRecord
     user.name = auth['info']['name']
     user.save
     user
+  end
+
+  def admin?
+    admin
   end
 end
