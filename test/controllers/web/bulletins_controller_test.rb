dd1fbada958391989c62ac69f25028aaa232daff
Vladimir Kozyrev <vkozyrev@anna.money>
2022-03-20 20:42:29 +0300

    update tests

--- test/controllers/web/bulletins_controller_test.rb
+++ test/controllers/web/bulletins_controller_test.rb
@@ -3,31 +3,23 @@
 require 'test_helper'
 
 class Web::BulletinsControllerTest < ActionDispatch::IntegrationTest
-  def setup
-    auth_hash = {
-      provider: 'github',
-      uid: '12345',
-      info: {
-        email: Faker::Internet.email,
-        name: Faker::Name.first_name
-      }
-    }
-    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash::InfoHash.new(auth_hash)
-    get callback_auth_url('github')
-  end
-
   test 'should get index' do
+    user = users(:regular_user)
+    sign_in(user)
     get bulletins_url
     assert_response :success
   end
 
   test 'should get new' do
+    user = users(:regular_user)
+    sign_in(user)
     get new_bulletin_url
     assert_response :success
   end
 
   test 'should create bulletin' do
     user = users(:regular_user)
+    sign_in(user)
     category = categories(:one)
     bulletin_title = Faker::Lorem.sentence
     bulletin_description = Faker::Lorem.paragraphs.join("\n")
--- test/controllers/web/categories_controller_test.rb
+++ test/controllers/web/categories_controller_test.rb
@@ -4,27 +4,9 @@ require 'test_helper'
 
 class Web::CategoriesControllerTest < ActionDispatch::IntegrationTest
   test 'should get index' do
+    user = users(:admin)
+    sign_in(user)
     get admin_categories_url
     assert_response :success
   end
-
-  # test 'should get new' do
-  #   get new_admin_category
-  #   assert_response :success
-  # end
-
-  # test 'should get create' do
-  #   get web_categories_create_url
-  #   assert_response :success
-  # end
-
-  # test 'should get edit' do
-  #   get web_categories_edit_url
-  #   assert_response :success
-  # end
-
-  # test 'should get destroy' do
-  #   get web_categories_destroy_url
-  #   assert_response :success
-  # end
 end
