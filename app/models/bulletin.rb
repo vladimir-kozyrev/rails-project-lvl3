1bc2a008a1c47f6f89d0fbbf7c512235c3509f17
Vladimir Kozyrev <vkozyrev@anna.money>
2022-03-20 20:45:04 +0300

    add aasm

--- app/models/bulletin.rb
+++ app/models/bulletin.rb
@@ -1,9 +1,27 @@
 # frozen_string_literal: true
 
 class Bulletin < ApplicationRecord
+  include AASM
+
   belongs_to :user
   belongs_to :category
   validates :image, file_size: { less_than_or_equal_to: 5.megabytes },
                     file_content_type: { allow: ['image/jpeg', 'image/png'] }
   has_one_attached :image
+
+  aasm column: 'state' do
+    state :draft, initial: true
+    state :under_moderation
+    state :published
+    state :rejected
+    state :archived
+
+    # event :publish do
+    #   transitions from: :under_moderation, to: :published
+    # end
+
+    # event :archive do
+    #   transitions from: [:published, :under_moderation], to: :archived
+    # end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_18_144456) do
+ActiveRecord::Schema.define(version: 2022_03_20_181113) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2022_03_18_144456) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "state"
     t.index ["category_id"], name: "index_bulletins_on_category_id"
     t.index ["user_id"], name: "index_bulletins_on_user_id"
   end
