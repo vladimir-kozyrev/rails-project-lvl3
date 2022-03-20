93fa74b47cbec7413a1afc39f190bcc544ea7246
Vladimir Kozyrev <vkozyrev@anna.money>
2022-03-20 20:26:10 +0300

    add pundit policy for Category

--- app/controllers/web/categories_controller.rb
+++ app/controllers/web/categories_controller.rb
@@ -2,16 +2,21 @@
 
 module Web
   class CategoriesController < ApplicationController
+    after_action :verify_authorized
+
     def index
       @categories = Category.all
+      authorize @categories
     end
 
     def new
       @category = Category.new
+      authorize @category
     end
 
     def create
       @category = Category.new(category_params)
+      authorize @category
       if @category.save
         redirect_to admin_categories, notice: 'Successfully created the category'
       else
@@ -21,10 +26,12 @@ module Web
 
     def edit
       @category = Category.find(params[:id])
+      authorize @category
     end
 
     def update
       @category = Category.find(params[:id])
+      authorize @category
       if @category.update(category_params)
         redirect_to admin_categories, notice: 'Successfully updated the category'
       else
@@ -34,6 +41,7 @@ module Web
 
     def destroy
       @category = Category.find(params[:id])
+      authorize @category
       @category.delete
       redirect_to admin_categories, notice: 'The category was deleted'
     end
