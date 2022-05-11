8a7f0764efe064b4776d26f419fa58906a725580
Vladimir Kozyrev <vkozyrev@anna.money>
2022-05-11 11:22:10 +0400

    make error messages consistent

--- app/controllers/web/bulletins_controller.rb
+++ app/controllers/web/bulletins_controller.rb
@@ -37,7 +37,7 @@ module Web
       @bulletin = Bulletin.find(params[:id])
       authorize @bulletin
     rescue Pundit::NotAuthorizedError
-      redirect_to root_path, alert: t('.failure')
+      redirect_to root_path, alert: t('web.auth.not_authorized')
     end
 
     def update
