1fed7a4585d394738c65b706eaba095dab7e0dd0
Vladimir Kozyrev <vkozyrev@anna.money>
2022-05-20 12:10:37 +0400

    move logic from views to helpers

--- app/helpers/web/admin_helper.rb
+++ app/helpers/web/admin_helper.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 module Web::AdminHelper
+  def admin_root_path_class
+    'active' if current_page?(admin_root_path)
+  end
+
+  def admin_bulletins_path_class
+    'active' if current_page?(admin_bulletins_path)
+  end
+
+  def admin_categories_path_class
+    'active' if current_page?(admin_categories_path)
+  end
 end
